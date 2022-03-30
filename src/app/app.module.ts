4fa5a41bd5c17ed862d8669b9b918f642c4fc67d
Richard Laurent <ronaldorichard27@gmail.com>
2022-03-30 14:22:34 +0700

    First Commit [Finishing Deisgn Movie App Design]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {
+        fontFamily: {
+        'bebas': ['Bebas Neue'],
+        'della': ['Dela Gothic One']
+      }
+    },
+  },
+  plugins: [],
+}
